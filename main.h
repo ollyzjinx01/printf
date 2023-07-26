c392fd2e91c6eccc8e21bee0e7ac363052580970
Babatunde Ajibade <tunde4etin@yahoo.com>
2023-07-25 18:35:41 -0700

    handled d and i specifier

--- main.h
+++ main.h
@@ -7,4 +7,5 @@
 int _printf(const char *format, ...);
 void print_char(char ch, int *charCount);
 void print_string(va_list args, int *charCount);
+void print_integer(int num, int *charCount);
 #endif /* MAIN_H */
